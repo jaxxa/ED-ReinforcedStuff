f54b3f2cc6d33ac7f684a3bef1da408e0759853d
Jaxxa <jaxxa90@gmail.com>
2016-04-13 07:50:52 +1000

    Initial Test for building creating Stuff

---
+++ Source/ED-ReinforcedStuff/Building_MolecularReinforcmentCompressor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+using RimWorld;
+using Verse;
+
+namespace ED_ReinforcedStuff
+{
+    public class Building_MolecularReinforcmentCompressor : Building
+    {
+
+        public override void TickRare()
+        {
+            Log.Message("TickLong");
+
+            base.TickRare();
+            Thing _NewResource = ThingMaker.MakeThing(ThingDef.Named("PlasteelReinforced"));
+
+                     // Meal meal = (Meal)ThingMaker.MakeThing(ThingDefOf.MealNutrientPaste, (ThingDef)null);
+            GenPlace.TryPlaceThing(_NewResource, this.InteractionCell, ThingPlaceMode.Near);
+            
+        }
+    }
+}
